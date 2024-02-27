0db71fed72fbf55ac46eb326ffe607fe5ea15f5d
dmacisso <dmacisso@gmail.com>
2024-02-27 18:25:39 -0500

    Fix bug. Remove .trim() from userMessage.trim() in input fild. add it userMessage before sending to Puser

--- lib/chat.jsx
+++ lib/chat.jsx
@@ -50,11 +50,14 @@ export default function Chat() {
     fetch('/admin/send-chat', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ message: userMessage, socket_id: socketId }),
+      body: JSON.stringify({
+        message: userMessage.trim(),
+        socket_id: socketId,
+      }),
     });
     setMessageLog((prev) => [
       ...prev,
-      { selfMessage: true, message: userMessage },
+      { selfMessage: true, message: userMessage.trim() },
     ]);
     setUserMessage('');
   }
